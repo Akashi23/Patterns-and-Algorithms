160a8430a8a5193e5e924ca0851ac273b97a4130
Diniyar <d.zhagparov@galamat.biz>
2023-08-07 11:15:44 +0600

    2d search

--- leetcode/search_a_2d_matrix.py
+++ leetcode/search_a_2d_matrix.py
@@ -3,29 +3,89 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            if matrix[i][0] <= target <= matrix[i][-1]:
-                if self.binarySearch(matrix[i], target) != -1:
-                    return True
-
+        array_vertical = [matrix[i][0] for i in range(len(matrix))]
+        index = self.findClosest(array_vertical, len(array_vertical), target)
+        if self.binarySearch(matrix[index], target) != -1:
+            return True
         return False
 
+    def findClosest(self, arr, n, target):
+        # Corner cases
+        if target <= arr[0]:
+            return 0
+        if target >= arr[n - 1]:
+            return n - 1
+
+        # Doing binary search
+        i = 0
+        j = n
+        mid = 0
+        while i < j:
+            mid = (i + j) // 2
+
+            if arr[mid] == target:
+                return mid
+
+            # If target is less than array
+            # element, then search in left
+            if target < arr[mid]:
+                # If target is greater than previous
+                # to mid, return closest of two
+                if mid > 0 and target > arr[mid - 1]:
+                    return self.getClosest(arr, mid - 1, mid, target)
+
+                # Repeat for left half
+                j = mid
+
+            # If target is greater than mid
+            else:
+                if mid < n - 1 and target < arr[mid + 1]:
+                    return self.getClosest(arr, mid, mid + 1, target)
+
+                # update i
+                i = mid + 1
+
+        # Only single element left after search
+        return mid
+
+    def getClosest(self, arr, val1, val2, target):
+        if target - arr[val1] > 0:
+            return val1
+        else:
+            return val2
+
     def binarySearch(self, nums: List[int], target: int) -> bool:
-        left, middle, right = 0, round(len(nums)/2), len(nums)
-        print(nums)
-        while (right != left):
-            if (nums[middle] < target):
-                right = middle
-                middle = round((right - left) / 2)
-            elif (nums[middle] > target):
-                left = middle
-                middle = round((right - left) / 2)
-            if nums[middle] == target:
-                return middle
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = int((left + right) / 2)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
         return -1
 
 
 solution = Solution()
-answer = solution.searchMatrix(
-    [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 34)
+answer = solution.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 34)
 print(answer)
+assert answer == True
+
+answer = solution.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]], 7)
+print(answer)
+assert answer == True
+
+answer = solution.searchMatrix([[1]], 0)
+print(answer)
+assert answer == False
+
+answer = solution.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3)
+print(answer)
+assert answer == True
+
+solution = Solution()
+answer = solution.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20]], 13)
+print(answer)
+assert answer == False
